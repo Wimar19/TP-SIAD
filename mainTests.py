fedc76ec5c36e147f5fab06afeab1bef0d5ad428
William <william.marceau.1@ulaval.ca>
2024-03-04 11:08:08 -0500

    Aller

---
+++ mainTests.py
@@ -0,0 +1,32 @@
+import problem as prob
+import solution as sol
+import solver
+import fastroute_problem as frp
+import route_solution as rsol
+
+
+print('Test des constructeurs des classes de base:')
+my_prob = prob.Problem()
+my_sol = sol.Solution()
+my_solver = solver.Solver()
+
+
+print('\nTest des constructeurs des classes spécialisées:')
+my_frp = frp.FastRouteProb()
+my_rsol = rsol.Route()
+
+
+print('\nTest des fonctions d\'une solution:')
+my_sol.evaluate()
+my_sol.validate()
+
+
+print('\nTest des fonctions d\'une route:')
+my_rsol.evaluate()
+my_rsol.validate()
+
+
+print('\nTest du polymorphisme:')
+for sol in (my_sol, my_rsol):
+    sol.evaluate()
+    sol.validate()
